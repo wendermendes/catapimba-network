da92ebb7a2e08b27b08d5fd538e994300ec96ea2
wendermendes <105708282+wendermendes@users.noreply.github.com>
2022-12-12 19:51:36 -0300

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,20 +1,54 @@
-module "modulo_tcloud_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+resource "aws_vpc" "terraform-vpc" {
+  cidr_block = "10.0.0.0/16"
 
-  name = "tcloud_vpc"
-  cidr = "172.16.0.0/16"
+tags = {
+    Name = "Terraform-VPC"
+  }
+}
 
-  azs = ["us-east-1a"]
-  private_subnets = ["172.16.1.0/24"]
-  public_subnets = ["172.16.2.0/24"]
-
-  enable_nat_gateway     = false
-  single_nat_gateway     = false
-  one_nat_gateway_per_az = false
+resource "aws_subnet" "private-subnet-1a" {
+  vpc_id     = aws_vpc.terraform-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
-    Terraform   = "true"
-    Environment = "producao"
-    Name        = "tcloud_vpc"
+    Name = "Private-Subnet-1a"
+  }
+}
+
+resource "aws_subnet" "public-subnet-1a" {
+  vpc_id     = aws_vpc.terraform-vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Public-Subnet-1a"
+  }
+}
+
+resource "aws_internet_gateway" "terraform-vpc-igw" {
+  vpc_id = aws_vpc.terraform-vpc.id
+
+  tags = {
+    Name = "Terraform VPC IGW"
+  }
+}
+
+resource "aws_internet_gateway_attachment" "vpc-igw-attach" {
+  count = 0
+  internet_gateway_id = aws_internet_gateway.terraform-vpc-igw.id
+  vpc_id = aws_vpc.terraform-vpc.id
+}
+
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.terraform-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.terraform-vpc-igw.id
+  }
+
+  tags = {
+    Name = "Public-RT"
   }
 }
